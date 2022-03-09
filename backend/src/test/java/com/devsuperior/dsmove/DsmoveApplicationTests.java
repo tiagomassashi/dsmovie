387bb1f34e8b900c986bd1185ea06806c6ffcba7
Tiago Massashi Nagata <59301876+tiagomassashi@users.noreply.github.com>
2022-03-08 22:30:29 -0300

    Projeto criado

---
+++ backend/src/test/java/com/devsuperior/dsmove/DsmoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
