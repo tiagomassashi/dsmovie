5e0a4bcc120fbd0edc5766c0893d50e6996451d5
Tiago Massashi Nagata <59301876+tiagomassashi@users.noreply.github.com>
2022-03-09 19:09:24 -0300

    Security config

--- backend/src/main/java/com/devsuperior/dsmove/DsmoveApplication.java
+++ backend/src/main/java/com/devsuperior/dsmove/DsmoveApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DsmoveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DsmoveApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DsmoveApplication.class, args);
+  }
 }
